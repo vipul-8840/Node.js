07258e92f35782eb75c6510fda9aad23d38ca430
vipul-8840 <vipulkumarlen@gmail.com>
2025-04-05 10:44:51 +0530

    creating http server

--- server.js
+++ server.js
@@ -1,5 +1,14 @@
-const express = require('express');
-const server = express();
-server.listen(8080,()=>{
-     console.log(`server listen port no : ${8080}`);
-});
+// const express = require('express');
+// const server = express();
+// server.listen(8080,()=>{
+//      console.log(`server listen port no : ${8080}`);
+// });
+// const http = require('http');
+// const server = http.createServer((req,res)=>{
+//       console.log("server started ");
+//       res.end("hello")
+// })
+// console.log("hello world");
+// server.listen(8000,()=>{
+//      console.log(`server listend this port no 8000`);
+// });
