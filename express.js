8ccf669efc7f7fcbb19bbeb25d05cc7a2b9fd174
vipul-8840 <vipulkumarlen@gmail.com>
2025-04-08 15:02:38 +0530

    middleware

--- express.js
+++ express.js
@@ -1,8 +1,14 @@
 const express = require("express");
 const path =require('path')
 const app = express();
+function applyMiddleware(req,res,next){
+       console.log(req.method,req.ip,req.hostname);
+       next();
+}
+app.use(applyMiddleware);
 app.get('/',(req,res)=>{
-    res.sendFile(path.join(__dirname, "index.js"));;
+    res.sendFile(path.join(__dirname, "index.js"));
+   
 })
 app.listen(8000,()=>{
     console.log(`server is runninr port no 8000`);
